046ef259646ca534d70ea4e1e2cca34707cbc3fe
Keppla <benjamin.koeppchen@triplet.gmbh>
2023-02-25 12:56:26 +0100

    removed svelte-kit cached files

--- frontend/.svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/editors/[editor]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/editors/[editor]/+page.svelte";
